963113cdae879c047c5703b9169317b6a3b87d04
Ostap4ello <okpeleh@gamil.com>
2024-12-20 12:55:40 +0100

    fix: added success prompt

---
+++ project/tm_formal.py
@@ -0,0 +1,85 @@
+import turing_machine
+
+
+def write(turing: turing_machine.TuringMachine, file_name: str):
+    with open(file_name, "w") as f:
+        f.write("TM1 = (Q, T, B, Σ, δ, q0, F)\n")
+
+        row = "Q = { "
+        for state in turing.states:
+            row += state + " "
+        row += "}\n"
+        f.write(row)
+
+        row = "T = { "
+        for symbol in turing.tape_alphabet:
+            row += symbol + " "
+        row += "}\n"
+        f.write(row)
+
+        f.write("B = ''\n")
+
+        row = "Σ = { "
+        for symbol in turing.input_alphabet:
+            row += symbol + " "
+        row += "}\n"
+        f.write(row)
+
+        row = "δ = {\n"
+        for (oldstate, input), (newstate, output, direction) \
+                in turing.transition_table.items():
+            row += f"    ({oldstate}, {input}) -> \
+                ({newstate}, {output}, {direction})\n"
+        row += "}\n"
+        f.write(row)
+
+        f.write(f"q0 = {turing.start_state}\n")
+
+        row = "F = { "
+        for state in turing.accept_state:
+            row += state + " "
+        row += "}\n"
+        f.write(row)
+
+        print(f"tm_formal: data written to {file_name}")
+
+
+# def write_old(turing: dict[str, Any], file_name: str):
+#     with open(file_name, "w") as f:
+#         f.write("TM1 = (Q, T, B, Σ, δ, q0, F)\n")
+#
+#         row = "Q = { "
+#         for state in turing["states"]:
+#             row += state + " "
+#         row += "}\n"
+#         f.write(row)
+#
+#         trw = "T = { "
+#         for symbol in turing["tape_alphabet"]:
+#             trw += symbol + " "
+#         trw += "}\n"
+#         f.write(trw)
+#
+#         f.write("B = ''\n")
+#
+#         orw = "Σ = { "
+#         for symbol in turing["input_alphabet"]:
+#             orw += symbol + " "
+#         orw += "}\n"
+#         f.write(orw)
+#
+#         row = "δ = {\n"
+#         for (oldstate, input), (newstate, output, direction) \
+#                 in turing["transition_table"].items():
+#             row += f"    ({oldstate}, {input}) -> \
+#                 ({newstate}, {output}, {direction})\n"
+#         row += "}\n"
+#         f.write(row)
+#
+#         f.write(f"q0 = {turing['start_state']}\n")
+#
+#         row = "F = { "
+#         for state in turing["accept_states"]:
+#             row += state + " "
+#         row += "}\n"
+#         f.write(row)
